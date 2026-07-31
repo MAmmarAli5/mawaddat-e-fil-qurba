99fbf596e09946c6ea3ec97990a1b47b9fdee7d9
MAmmarAli5 <MawaddatfilQurba5@gmail.com>
2026-07-31 13:29:02 +0500

    Add Firebase integration for student search results

---
+++ js/result.js
@@ -0,0 +1,82 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    get
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyB3FCQ0PFQaQDwdjIvvVd3shQ_EXqL3iMA",
+    authDomain: "mawaddat-fil-qurba.firebaseapp.com",
+    databaseURL: "https://mawaddat-fil-qurba-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mawaddat-fil-qurba",
+    storageBucket: "mawaddat-fil-qurba.firebasestorage.app",
+    messagingSenderId: "637175775327",
+    appId: "1:637175775327:web:95da7d655c7606f5ef9bea"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const searchBtn = document.getElementById("searchBtn");
+const resultBox = document.getElementById("resultBox");
+
+searchBtn.addEventListener("click", async () => {
+
+    const studentName = document
+        .getElementById("studentSearch")
+        .value
+        .trim()
+        .toLowerCase();
+
+    if (studentName === "") {
+
+        alert("Please enter student name.");
+        return;
+
+    }
+
+    const snapshot = await get(ref(db, "results"));
+
+    let found = false;
+
+    resultBox.innerHTML = "";
+
+    snapshot.forEach((child) => {
+
+        const data = child.val();
+
+        if (data.studentName.toLowerCase() === studentName) {
+
+            found = true;
+
+            resultBox.innerHTML = `
+                <h2>Student Result</h2>
+
+                <p><strong>Name:</strong> ${data.studentName}</p>
+
+                <p><strong>Marks:</strong>
+                ${data.score} / ${data.total}</p>
+
+                <p><strong>Percentage:</strong>
+                ${data.percentage}%</p>
+
+                <p><strong>Date:</strong>
+                ${data.date}</p>
+            `;
+
+        }
+
+    });
+
+    if (!found) {
+
+        resultBox.innerHTML =
+        "<h2>Student not found.</h2>";
+
+    }
+
+});
