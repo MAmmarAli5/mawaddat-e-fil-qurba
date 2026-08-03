fe59c660582ebea9aa141db661375764b0f6a021
MAmmarAli5 <MawaddatfilQurba5@gmail.com>
2026-08-03 23:30:29 +0500

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,16 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
     getAuth,
     signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-const loginBtn = document.getElementById("loginBtn");
+
+// Firebase Config یہاں
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3FCQ0PFQaQDwdjIvvVd3shQ_EXqL3iMA",
+    authDomain: "mawaddat-fil-qurba.firebaseapp.com",
+    databaseURL: "https://mawaddat-fil-qurba-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mawaddat-fil-qurba",
+    storageBucket: "mawaddat-fil-qurba.firebasestorage.app",
+    messagingSenderId: "637175775327",
+    appId: "1:637175775327:web:95da7d655c7606f5ef9bea"
+   // آپ کی Config
+};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const loginForm = document.getElementById("loginForm");
-
 const email = document.getElementById("email");
-
 const password = document.getElementById("password");
-   
