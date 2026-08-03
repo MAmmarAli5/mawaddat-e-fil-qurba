e305a7acfad252c2b105240c987f8b985bbedbe2
MAmmarAli5 <MawaddatfilQurba5@gmail.com>
2026-08-03 22:32:52 +0500

    Update question-bank.js

--- js/question-bank.js
+++ js/question-bank.js
@@ -25,9 +25,7 @@ const firebaseConfig = {
     appId: "1:637175775327:web:95da7d655c7606f5ef9bea"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
+import { db } from "../firebase/config.js";
 // ===============================
 // HTML ELEMENTS
 // ===============================
