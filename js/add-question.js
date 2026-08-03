7224f1fb4e8607e7312db59f4219c97bc71cd474
MAmmarAli5 <MawaddatfilQurba5@gmail.com>
2026-08-03 22:18:48 +0500

    Update add-question.js

--- js/add-question.js
+++ js/add-question.js
@@ -86,3 +86,55 @@ saveBtn.addEventListener("click", () => {
     answer.value = "";
 
 });
+// ===============================
+// SAVE QUESTION
+// ===============================
+
+const form = document.getElementById("questionForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const question = document.getElementById("question").value;
+
+    const option1 = document.getElementById("option1").value;
+
+    const option2 = document.getElementById("option2").value;
+
+    const option3 = document.getElementById("option3").value;
+
+    const option4 = document.getElementById("option4").value;
+
+    const answer = document.getElementById("answer").value;
+
+    const questionData = {
+
+        question: question,
+
+        options: [
+            option1,
+            option2,
+            option3,
+            option4
+        ],
+
+        answer: Number(answer)
+
+    };
+
+    try {
+
+        await push(ref(db, "questions"), questionData);
+
+        alert("✅ Question Added Successfully!");
+
+        form.reset();
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
